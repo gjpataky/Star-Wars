06eabf76aee16a0ec9347af4cc422510dc46884e
gjpataky <gjpataky@yahoo.com>
2019-11-21 20:59:27 -0600

    initial

---
+++ server1.js
@@ -0,0 +1,85 @@
+// Dependencies
+// ===========================================================
+var express = require("express");
+const path = require('path');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+//set up the express app to handle data parsing
+app.use(express.urlencoded({ extend: true }));
+app.use(express.json());
+
+// Data
+// ===========================================================
+const characters = [
+  {
+    routeName: "yoda",
+    name: "Yoda",
+    role: "Jedi Master",
+    age: 900,
+    forcePoints: 2000
+
+  },
+  {
+    routeName: "darthmaul",
+    name: "Darth Maul",
+    role: "Sith Lord",
+    age: 200,
+    forcePoints: 1200
+  },
+];
+
+
+// Routes
+// ===========================================================
+//general route
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, 'view.html'));
+});
+
+//add route
+app.get("/add", function (req, res) {
+  res.sendFile(path.join(__dirname, 'add.html'));
+});
+
+//get all the dataz
+app.get('/api/characters', (req, res) => {
+  return res.json(characters);
+});
+
+// get one object from dataz
+app.get('/api/characters/:character', (req, res) => {
+  const chosen = req.params.character;
+
+  const chosenOne = characters.filter(obj => {
+    return obj.routeName === chosen;
+  });
+
+  if (chosenOne.length) {
+    return res.json(chosenOne[0]);
+  }
+
+  return res.send('character, i do not see.');
+});
+
+// add an object to the dataz
+app.post('/api/characters', (req, res) => {
+  const newCharacter = req.body;
+  console.log(newCharacter);
+
+  newCharacter.routeName = newCharacter.name.replace(/\s+/g, '').toLowerCase();
+
+  // add new character to db
+  characters.push(newCharacter);
+
+  //send back what I just added
+  res.json(newCharacter);
+});
+
+/*find object in db based on routeNmae*/
+
+// Listener
+// ===========================================================
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
